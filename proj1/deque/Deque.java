a037e3018e5b83eaad69873cbb601fcf03144a6e
steven4225 <1614417712@qq.com>
2024-11-15 12:48:28 +0800

    Update Deque.java

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -1,16 +1,21 @@
 package deque;
 
-public  interface Deque <T>{
-    void addfirst(T item);
-    void addlast(T item);
-    default boolean isempty(){
-        return size()==0;
+public interface Deque<T> {
+    void addFirst(T item);
 
+    void addLast(T item);
+
+    default boolean isEmpty() {
+        return size() == 0;
     }
-    int size();
-    void printdeque();
-    T removefirst();
-    T removelast();
-    T get(int index);
 
+    int size();
+
+    void printDeque();
+
+    T removeFirst();
+
+    T removeLast();
+
+    T get(int index);
 }
